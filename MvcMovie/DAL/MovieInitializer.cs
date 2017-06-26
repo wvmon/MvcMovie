917dd5b6f78fd7723f25c3bb38edc5354c726edd
William Montesedoca <wvmon360@gmail.com>
2017-06-25 22:57:23 -0600

    Scaffolding and DbContext applied

---
+++ MvcMovie/DAL/MovieInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MvcMovie.Models;
+
+namespace MvcMovie.DAL
+{
+    public class MovieInitializer : DropCreateDatabaseIfModelChanges<MvcMovieContext>
+    {
+        protected override void Seed(MvcMovieContext context)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie{ID = 1050, Title = "Other Side of Heaven", ReleaseDate = DateTime.Now, Genre = "Chruch", Price = 1},
+                new Movie{ID = 4022, Title = "Best Two Years", ReleaseDate = DateTime.Now, Genre = "Chruch", Price = 1},
+                new Movie{ID = 4041, Title = "The RM", ReleaseDate = DateTime.Now, Genre = "Chruch", Price = 1},
+                new Movie{ID = 1045, Title = "Singles Ward", ReleaseDate = DateTime.Now, Genre = "Chruch", Price = 1},
+                new Movie{ID = 3141, Title = "Saints and Soldiers", ReleaseDate = DateTime.Now, Genre = "Chruch", Price = 1}
+            };
+            movies.ForEach(s => context.Movies.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MvcMovie/DAL/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using MvcMovie.Models;
+
+namespace MvcMovie.DAL
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext() : base("MvcMovieContext")
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
